d248f4dd31bed87eddb5d29e4dac1bf014465daa
Altman <595303122@qq.com>
2025-07-15 12:00:54 +0800

    硬件设备信息

---
+++ Jlib/HardWareDevice.cs
@@ -0,0 +1,28 @@
+﻿namespace Jlib
+{
+    public class HardWareDevice 
+    {
+        public string ProductId = "";
+        public string VendorId = "";
+        public string SerialNumber = "";
+        public EnumDeviceType DeviceType = EnumDeviceType.Unknown;
+        public string DeviceID
+        {
+            get
+            {
+                return $"{VendorId}-{ProductId}-{SerialNumber}";
+            }
+        }
+        public HardWareDevice(string vid, string pid,string snumer)
+        {
+            ProductId = pid;
+            VendorId = vid;
+            SerialNumber = snumer;
+        }
+        public void SetDeviceType(EnumDeviceType deviceType)
+        {
+            DeviceType = deviceType;
+        }
+
+    }
+}
